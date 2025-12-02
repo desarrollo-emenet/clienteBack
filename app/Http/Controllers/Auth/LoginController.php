844e4cf5bb01d83292c3a46f9c6d615791d0a752
mcidf12 <mcid653@gmail.com>
2025-12-02 11:09:34 -0600

    n

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,7 +36,6 @@ class LoginController extends Controller
             return response()->json(['message' => 'Cuenta no verificada'], 403);
         }
 
-
         //$user->tokens()->delete();
         $tokenName = $credentials['email'] ?? $credentials['cliente'];
         $token = $user->createToken($tokenName)->plainTextToken;
