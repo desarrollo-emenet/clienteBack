5952139e25211fc0b9ddf3323516ec15039241aa
Cesar Rivera <cesarivan25479@gmail.com>
2026-08-12 17:21:45 -0600

    apartado de recuperacion de contraseña

--- app/Http/Controllers/Auth/RecoveryPasswordController.php
+++ app/Http/Controllers/Auth/RecoveryPasswordController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Service\Auth\recoveryService;
-
+use App\Http\Requests\auth\recoveryRequest;
+use App\Http\Requests\auth\tokenRequest;
+use App\Http\Requests\auth\updateRequest;
 
 class RecoveryPasswordController extends Controller
 {
@@ -15,31 +17,33 @@ class RecoveryPasswordController extends Controller
         $this->recoveryService = $recoveryService;
     }
 
-    public function sendEmail(Request $request)
+    public function sendEmail(recoveryRequest $request)
     {
         try{
            return $this->recoveryService->sendEmail($request);
         }catch(\Throwable $e){
             return response()->json([
                 "status" => "false",
-                "message" => "Ocurrió un error al enviar el correo de recuperación. " . $e->getMessage()
+                "message" => "Ocurrió un error al enviar el correo de recuperación.",
+                "error" => $e->getMessage(),
             ], 500);
         }
     }
 
-    public function updatePassword(Request $request)
+    public function updatePassword(updateRequest $request)
     {
         try{
             return $this->recoveryService->updatePassword($request);
         }catch(\Throwable $e){
             return response()->json([
                 "status" => "false",
-                "message" => "Ocurrió un error al actualizar la contraseña. " . $e->getMessage()
+                "message" => "Ocurrió un error al actualizar la contraseña.",
+                "error" => $e->getMessage(),
             ], 500);
         }
     }
 
-    public function validarToken(Request $request){
+    public function validarToken(tokenRequest $request){
 
     try {
         return $this->recoveryService->validarToken($request);
--- app/Service/Auth/recoveryService.php
+++ app/Service/Auth/recoveryService.php
@@ -5,38 +5,29 @@ namespace App\Service\Auth;
 use App\Mail\RecoverPasswordMail;
 use App\Models\User;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 
 class recoveryService
 {
-    public static $rulesUpdate = [
-        'token' => 'required|string',
-        'password'  => 'required|string|min:8|confirmed',
-    ];
-
-    public function sendEmail(Request $request)
+    public function sendEmail($request)
     {
         $email = $request->input('email');
 
         // Verificar que el email exista en la base de datos
-        if (!$this->validateEmail($email)) {
-            return response()->json([
-                "status" => "false",
-                "message" => "Email no enocontrado"
-            ], 404);
-        }
+        if (!$this->validateEmail($request->email))  return response()->json([
+            "status" => "error",
+            "message" => "La dirección de correo electrónico no está registrada."
+        ], 404);
         //Log::info('Email encontrado en la base de datos: ' . $email);
 
         $this->send($email);
 
         return response()->json([
-            "status" => "true",
-            "message" => "Correo enviado correctamente",
+            "status" => "success",
+            "message" => "Se ha enviado un correo de recuperación a la dirección de correo electrónico proporcionada.",
             "email" => $email
         ], 200);
     }
@@ -87,60 +78,14 @@ class recoveryService
         );
     }
 
-
-    private function ObtenerTokenValidad(string $token)
+    public function validarToken($request)
     {
-
-        $passwordReset = DB::table('password_resets')
-            ->where('token', $token)
-            ->first();
-
-        //sino existe
-        if (!$passwordReset) {
-            response()->json([
-                'status' => false,
-                'messaje' => 'Ya se uso el enlace o no existe'
-            ], 400);
-            return null;
-        }
-
-        //tiempo de expiracion
-        $expire = config('auth.password.users.expire', 60);
-
-        //verificar tiempo
-        if (
-            Carbon::parse($passwordReset->created_at)
-            ->addMinutes($expire)
-            ->isPast()
-        ) {
-            // Eliminar el token expirado
-            DB::table('password_resets')
-                ->where('email', $passwordReset->email)
-                ->delete();
-
-            response()->json([
-                'status' => false,
-                'message' => 'El enlace ha expirado.',
-            ], 400);
-            return null;
-        }
-
-        return $passwordReset;
-    }
-    public function validarToken(Request $request)
-    {
-        $request->validate([
-            'token' => 'required|string'
-        ]);
-
         $resultado = $this->ObtenerTokenValidad($request->token);
 
-        if (!$resultado) {
-            return response()->json([
-                "status" => false,
-                "message" => "enlace invalido"
-            ], 400);
-        }
+        if (!$resultado) return response()->json([
+            "status" => false,
+            "message" => "Enlace invalido"
+        ], 400);
 
         return response()->json([
             "status" => true,
@@ -148,44 +93,47 @@ class recoveryService
         ], 200);
     }
 
-    public function updatePassword(Request $request)
+    private function ObtenerTokenValidad(string $token)
     {
-        $data = $request->validate(self::$rulesUpdate);
 
-        $tokenData = $this->ObtenerTokenValidad($data['token']);
+        $passwordReset = DB::table('password_resets')->where('token', $token)->first();
+        //sino existe
+        if (!$passwordReset) return null;
 
-        if (!$tokenData) {
-            return response()->json([
-                "status" => false,
-                "message" => "enlace invalido"
-            ], 400);
+        //tiempo de expiracion
+        $expire = config('auth.password.users.expire', 60);
+
+        //verificar tiempo
+        if (Carbon::parse($passwordReset->created_at)->addMinutes($expire)->isPast()) {
+            // Eliminar el token expirado
+            DB::table('password_resets')->where('email', $passwordReset->email)->delete();
+            return null;
         }
 
-        $token = $tokenData;
+        return $passwordReset;
+    }
 
+
+    public function updatePassword($request)
+    {
+        $token = $this->ObtenerTokenValidad($request->token);
+        if (!$token) return response()->json([
+            "status" => "error",
+            "message" => "El enlace anterior no es válido, envia una nueva solicitud"
+        ], 404);
         // Actualizar la contraseña del usuario
         $user = User::where('email', $token->email)->first();
+        if (!$user) return response()->json([
+            "status" => "error",
+            "message" => "Usuario no encontrado."
+        ], 404);
 
-        if (!$user) {
-            return response()->json([
-                "status" => "false",
-                "message" => "Usuario no encontrado."
-            ], 404);
-        }
+        $user->update(['password' => Hash::make($request->password)]);
+        DB::table('password_resets')->where('email', $token->email)->delete();
 
-        DB::transaction(function () use ($user, $data, $token) {
-
-            $user->update([
-                'password' => Hash::make($data['password'])
-            ]);
-
-            DB::table('password_resets')
-                ->where('email', $token->email)
-                ->delete();
-        });
-
+        DB::commit();
         return response()->json([
-            "status" => "true",
+            "status" => "success",
             "message" => "Contraseña actualizada correctamente."
         ], 200);
     }
